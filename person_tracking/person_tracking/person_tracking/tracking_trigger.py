e4a3b556b737fdd03788b3470b8fbb88251d1820
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2024-08-04 17:05:15 +0200

    Update tracking_trigger.py

--- person_tracking/person_tracking/person_tracking/tracking_trigger.py
+++ person_tracking/person_tracking/person_tracking/tracking_trigger.py
@@ -1,4 +1,3 @@
-
 #To handle ROS node
 import rclpy
 from rclpy.node import Node
@@ -6,17 +5,23 @@ from rclpy.node import Node
 #ROS image message
 from sensor_msgs.msg import Image
 
-#To handle images
-#import cv2
+#Landmarks messages
+from hand_gestures_msgs.msg import Landmarks, Landmark
 
-#To connect to the drone
-#from djitellopy import Tello, tello
+#person tracked messages
+from person_tracked.msg import PersonTracked
+
+#bounding boxes messages
+from all_bounding_boxes_msg.msg import AllBoundingBoxes
+
+#To handle images
+import cv2
 
 #To convert cv2 images to ROS Image messages
 from cv_bridge import CvBridge
 
 #Node base to be able to integrate our project to the Tello_ws
-from plugin_server_base.plugin_base import PluginBase
+#from plugin_server_base.plugin_base import PluginBase
 
 from ultralytics import YOLO
 
@@ -31,71 +36,102 @@ classes_ID = [k for k,v in classes.items() if v in classes_needed]
 #Detection threshold probability
 minimum_prob = 0.4  
 
-class TriggerTracking(PluginBase):
+right_hand_gesture = "Open_palm"
+
+left_hand_gesture = "Open_palm"
+class TriggerTracking(Node):
 
     #Topic names
-    hand_landmark = "/hand/landmark"
-    person_tracked = "/person/tracked"
-    image_raw= "/camera/image_raw"
-    all_detected_topic = "/person/all_detected"
+    hand_landmarks_topic = "/hand/landmarks"
+    person_tracked_topic = "/person_tracked"
+    #image_raw= "/camera/image_raw"
+    bounding_boxes_topic = "/all_bounding_boxes"
+
+
     #person_list_topic_name = "/persons_list"
     #detector_images_topic = "/image_person"
-    velocity_command_topic_name = "/cmd_vel"
-    land_topic_name = "/land"
-    takeoff_topic_name = "/takeoff" 
+    right_hand_gesture = "Open_Palm"
+
+    left_hand_gesture = "Open_Palm"
     
     def __init__(self,name):
         #Creating the Node
         super().__init__(name)
         
         #subscribers
-        self.sub_raw = self.create_subscription(Image,image_raw, self.listener_callback,10)
-        self.sub_landmark = self.create_subscription(Image,hand_landmark, self.spot_trigger,10)
+        self.sub_bounding_boxes = self.create_subscription(AllBoundingBoxes,self.bounding_boxes_topic, self.bounding_boxes_listener_callback,10)
+        self.sub_landmark = self.create_subscription(Landmarks,self.hand_landmarks_topic, self.landmarks_listener_callback,10)
         
         #publishers
-        self.publisher_all_detected = self.create_publisher(Image,all_detected_topic,10)
-        self.timer_1 = self.create_timer(0.1, self.all_detected_callback)
+        self.publisher_to_track= self.create_publisher(PersonTracked,self.person_tracked_topic,10)
+        self.timer_1 = self.create_timer(0.1, self.person_tracked_callback)
         
-        self.publisher_person_tracked = self.create_publisher(Image,person_tracked,10)
-        self.timer_2 = self.create_timer(0.1, self.person_tracked_callback)
 
         self.cv_bridge = CvBridge()
 
-        #output video initialization
-        #self.fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        #self.video = cv2.VideoWriter('~/output.avi',self.fourcc,20.0,(640,480))
+        #Variable to received bounding boxes containing all persons detected
+        self.boxes = None
 
-        #Variable to contain the frame coming directly from the drone
-        self.image_raw = None
+        #self.height = None
 
-        #Variable to read each frame. It contains all persons detected
-        self.image_all_detected = None
+        #self.width = None
         
         #Variable to contain only the image of target person 
-        self.image_target = None
+        #self.image_target = None
+
+        #Variable to contain landmarks messages
+        self.landmarks = None
+
+        self.person_tracked_middlepoint = None
+
+        self.person_tracked_left_hand_point = None
+
+        self.person_tracked_right_hand_point = None
+
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.video = cv2.VideoWriter('/media/maeri/UBUNTUUU/output670.mp4',self.fourcc,20.0,(640,480))
 
 
 ###########################first subscriber###########################################################################################   
-    def listener_callback(self, img):
-        """Callback function for the subscriber node (to topic /camera/image_raw).
-        For each image received, save in the log that an image has been received.
-        Then convert that image into cv2 format, perform tracking on that image"""
+    def bounding_boxes_listener_callback(self, boxes_msg):
+        """Callback function for the subscriber node (to topic /all_detected).
+        For each frame received, save in the log that an image has been received.
+        Then convert that image into cv2 format and keep that frame in a variable for further processing"""
         
-        self.get_logger().info('I saw an image')
-        self.image_raw = self.cv_bridge.imgmsg_to_cv2(img,'rgb8')
-        self.image_all_detected = self.detection(image)
-        ##########################################
-        ##Add code to track a single person.
-        #########################################
-        #self.video.write(np.array(self.image))
+        self.get_logger().info('Bounding boxes received')
+        self.boxes = boxes_msg
+        print
+       
         
-    def detection(self,frame):
-        """Function to perform object detection on frames"""
-        results = model.track(frame, persist=True, classes=classes_ID, conf=minimum_prob)
-        frame_ = results[0].plot()
-        return frame_
+        
+########################### second subscriber #########################################################################################
+    def landmarks_listener_callback(self, lndmrk):
+        """Callback function for the subscriber node (to topic /hand/landmarks).
+        Receives a landmark from the hand gesture plugin and saves that landmark in a variable for further processing."""
+        
+        self.get_logger().info('Landmark received')
+        self.landmarks = lndmrk
+        
 
-###########################second subscriber#########################################################################################
+
+
+######################### Publisher #####################################################################################################
+    def person_tracked_callback(self):
+        """This function listens to the /hand/landmarks topic, and waits to spot the person who did the triggering move. I
+        n case a person did the trigger move, the function calls a publish the frames with only that person tracked. It sends the coordinates of the 
+        center of the bounding boxes around that person to person_tracker node, so that the latter node sends commands to the drone to follow that person."""
+        #self.publisher_all_detected.publish(self.cv_bridge.cv2_to_imgmsg(np.array(self.image_all_detected), 'rgb8')) 
+        if self.person_tracked_middlepoint is None and self.landmarks is not None :
+            if self.landmarks.right_hand.gesture == self.right_hand_gesture and self.landmarks.left_hand.gesture == self.left_hand_gesture:
+                print("\n Open hand!! ")
+                self.person_tracked_left_hand_point = self.landmarks.left_hand.normalized_landmarks[0]
+                self.person_tracked_right_hand_point = self.landmarks.right_hand.normalized_landmarks[0]
+                print("\n Beware",self.person_tracked_left_hand_point)
+                print(self.person_tracked_right_hand_point,"\n\n")
+                print("Bounding boxes",self.boxes.bounding_boxes,"\n\n")
+                #self.video.write(cv2.line(self.image_all_detected,(int(middle_left.x*self.width),int(middle_left.y*self.height)),(int(middle_right.x*self.width),int(middle_right.y*self.height)),(255,0,0),4))
+            
+    
     def track_target(self,frame,id_of_interest):
         """Function to track a person of a certain id only on a frame"""
 
@@ -115,25 +151,7 @@ class TriggerTracking(PluginBase):
         else:
             return frame
 
-    def spot_trigger_callback(self,frame):
-        """This function listens to the /hand/landmarks topic, and waits to spot the person who did the triggering move. I
-        n case a person did the trigger move, the function calls a publish the frames with only that person tracked. It sends the coordinates of the 
-        center of the bounding boxes around that person to person_tracker node, so that the latter node sends commands to the drone to follow that person."""
-        pass
-
-########################first publisher#####################################################################################  
-    
-        
-    def all_detected_callback(self):
-        """
-        callback funtion for the publisher node (to topis /camera/image_detected).
-        The image on which object detection has been performed (self.image) is published on the topic '/camera/image_raw'
-        """
-        self.publisher_all_detected.publish(self.cv_bridge.cv2_to_imgmsg(np.array(self.image_all_detected), 'rgb8')) 
-        
-  
-    
-   
+       
 	    
     
 ###################################################################################################################################       
@@ -143,12 +161,14 @@ class TriggerTracking(PluginBase):
 def main(args=None):
     #Intialization ROS communication 
     rclpy.init(args=args)
-    trigger_tracking = TriggerTracking('trigger_tracking')
+    trigger_tracking = TriggerTracking('trigger_tracking_node')
 
     #execute the callback function until the global executor is shutdown
-    rclpy.spin(image_publisher)
+    rclpy.spin(trigger_tracking)
+    
 
+    trigger_tracking.video.release()
     #destroy the node. It is not mandatory, since the garbage collection can do it
-    image_publisher.destroy_node()
+    trigger_tracking.destroy_node()
     
     rclpy.shutdown()        
