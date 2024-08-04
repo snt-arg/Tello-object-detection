9371e5b33e5b8cd0e13bbe303041226b6f4b5aad
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2024-08-04 17:04:49 +0200

    Update detect_all.py

--- person_tracking/person_tracking/person_tracking/detect_all.py
+++ person_tracking/person_tracking/person_tracking/detect_all.py
@@ -5,11 +5,7 @@ from rclpy.node import Node
 #ROS image message
 from sensor_msgs.msg import Image
 
-#To handle images
-#import cv2
-
-#To connect to the drone
-#from djitellopy import Tello, tello
+from all_bounding_boxes_msg.msg import AllBoundingBoxes, Box
 
 #To convert cv2 images to ROS Image messages
 from cv_bridge import CvBridge
@@ -31,29 +27,27 @@ classes = model.names
 classes_needed = ["person"]
 classes_ID = [k for k,v in classes.items() if v in classes_needed]
 
-#Detection threshold probability
-minimum_prob = 0.4  
-
-#Topic names
-image_raw = "/camera/image_raw"
-all_detected_topic = "/all_detected"
-
 class DetectAll(PluginBase):
+    #Detection threshold probability
+    minimum_prob = 0.4  
 
+    #Topic names
+    image_raw_topic = "/camera/image_raw"
+    all_detected_topic = "/all_detected"
+    bounding_boxes_topic = "/all_bounding_boxes"
     
     def __init__(self,name):
         #Creating the Node
         super().__init__(name)
         
         #subscribers
-        self.sub_raw = self.create_subscription(Image,image_raw, self.listener_callback,10)
+        self.sub_raw = self.create_subscription(Image,self.image_raw_topic, self.listener_callback,10)
         #self.sub_raw = self.create_subscription(Image,image_raw, self.listener_callback,1)
 
         
         #publishers
-        self.publisher_all_detected = self.create_publisher(Image,all_detected_topic,10)
-        #self.publisher_all_detected = self.create_publisher(Image,all_detected_topic,1)
-        #self.timer_ = self.create_timer(0.1, self.all_detected_callback)
+        self.publisher_all_detected = self.create_publisher(Image,self.all_detected_topic,10)
+        self.publisher_bounding_boxes = self.create_publisher(AllBoundingBoxes,self.bounding_boxes_topic,10)
         
 
         self.cv_bridge = CvBridge()
@@ -63,6 +57,11 @@ class DetectAll(PluginBase):
 
         #Variable to read each frame. It contains all persons detected
         self.image_all_detected = None
+
+        #Variable containing all bounding boxes coordinates for a single frame
+        self.boxes = AllBoundingBoxes()
+
+
 
 
 ########################### Subscriber ###########################################################################################   
@@ -75,12 +74,22 @@ class DetectAll(PluginBase):
         self.get_logger().info('I saw an image')
         self.image_raw = self.cv_bridge.imgmsg_to_cv2(img,'rgb8')
         self.image_all_detected = self.detection(self.image_raw)
-        print("image,", self.image_raw)
+        
 
         
     def detection(self,frame):
         """Function to perform person object detection on a single frame"""
-        results = model.track(frame, persist=True, classes=classes_ID, conf=minimum_prob)
+        results = model.track(frame, persist=True, classes=classes_ID, conf=self.minimum_prob)
+        #prepare message 
+        box_msg = Box()
+        self.boxes = AllBoundingBoxes()
+        for box in results[0].boxes.xyxyn.tolist():
+            box_msg.top_left.x = box[0]
+            box_msg.top_left.y = box[1]
+            box_msg.bottom_right.x = box[2]
+            box_msg.bottom_right.y = box[3]
+            self.boxes.bounding_boxes.append(box_msg)
+
         frame_ = results[0].plot()
         return frame_
 
@@ -95,23 +104,17 @@ class DetectAll(PluginBase):
         #self.publisher_all_detected.publish(self.cv_bridge.cv2_to_imgmsg(np.array(self.image_all_detected), 'rgb8')) 
         if(self.image_all_detected is None):
             #self.publisher_all_detected.publish(self.cv_bridge.cv2_to_imgmsg(self.image_all_detected, 'rgb8')) 
-            print(self.image_all_detected)
+            self.get_logger().info('No image seen')
             
         else:
             #print(self.image_all_detected)
             self.publisher_all_detected.publish(self.cv_bridge.cv2_to_imgmsg(self.image_all_detected, 'rgb8')) 
-        
+            self.publisher_bounding_boxes.publish(self.boxes)
+
     def tick(self) -> NodeState:
         """This method is a mandatory for PluginBase node. It defines what we want our node to do.
         It gets called 20 times a second if state=RUNNING
         """
-
-        #self.get_logger().info("Tick")
-
-        #self.get_logger().info("Evaluate")
-
-        #self.get_logger().info("Command")
-        #self.listener_callback()
         self.all_detected_callback()
         return NodeState.RUNNING
 
