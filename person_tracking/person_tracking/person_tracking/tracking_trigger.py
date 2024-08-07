a9fbd5cd3b2edbe91d8612c0c6947a615b9570c3
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2024-08-07 13:52:59 +0200

    Update tracking_trigger.py

--- person_tracking/person_tracking/person_tracking/tracking_trigger.py
+++ person_tracking/person_tracking/person_tracking/tracking_trigger.py
@@ -3,7 +3,7 @@ import rclpy
 from rclpy.node import Node
 
 #ROS image message
-from sensor_msgs.msg import Image
+#from sensor_msgs.msg import Image
 
 #Landmarks messages
 from hand_gestures_msgs.msg import Landmarks, Landmark
@@ -15,46 +15,27 @@ from person_tracked.msg import PersonTracked, PointMsg
 from all_bounding_boxes_msg.msg import AllBoundingBoxes
 
 #To handle images
-import cv2
+#import cv2
 
 #To convert cv2 images to ROS Image messages
 from cv_bridge import CvBridge
 
-#Node base to be able to integrate our project to the Tello_ws
-#from plugin_server_base.plugin_base import PluginBase
 
-from ultralytics import YOLO
-
-#load the object detection model
-model = YOLO('yolov8n.pt') 
-
-#Filtering our classes of interest
-classes = model.names
-classes_needed = ["person"]
-classes_ID = [k for k,v in classes.items() if v in classes_needed]
-
-#Detection threshold probability
-minimum_prob = 0.4  
 
 right_hand_gesture = "Open_palm"
 
 left_hand_gesture = "Open_palm"
+
 class TriggerTracking(Node):
 
     #Topic names
     hand_landmarks_topic = "/hand/landmarks"
     person_tracked_topic = "/person_tracked"
-    #image_raw= "/camera/image_raw"
     bounding_boxes_topic = "/all_bounding_boxes"
 
-
-    #person_list_topic_name = "/persons_list"
-    #detector_images_topic = "/image_person"
-    right_hand_gesture = "Open_Palm"
-
-    left_hand_gesture = "Open_Palm"
     
     def __init__(self,name):
+
         #Creating the Node
         super().__init__(name)
         
@@ -62,161 +43,184 @@ class TriggerTracking(Node):
         self.sub_bounding_boxes = self.create_subscription(AllBoundingBoxes,self.bounding_boxes_topic, self.bounding_boxes_listener_callback,10)
         self.sub_landmark = self.create_subscription(Landmarks,self.hand_landmarks_topic, self.landmarks_listener_callback,10)
         
-        self.test_sub = self.create_subscription(Image,"/all_detected",self.test_listener,10)
+        """self.test_sub = self.create_subscription(Image,"/all_detected",self.test_listener,10)"""
 
         #publishers
         self.publisher_to_track= self.create_publisher(PersonTracked,self.person_tracked_topic,10)
         self.timer_1 = self.create_timer(0.1, self.person_tracked_callback)
         
-
+        #Used to convert cv2 frames into ROS Image messages and vice versa
         self.cv_bridge = CvBridge()
 
-        #Variable to received bounding boxes containing all persons detected
+        #Variable to receive bounding boxes containing all persons detected
         self.boxes = None
 
-        #Variable to contain landmarks messages
+        #Variable to contain received landmarks messages
         self.landmarks = None
 
-        self.person_tracked_middlepoint = None
+        #variable to contain the midpoint of the bounding box around the person we want to track
+        self.person_tracked_midpoint = None
 
+        #variables to contain the points located at the center of the left and right hand wrists of the person who performed the triggering gestures
         self.person_tracked_left_hand_point = None
 
         self.person_tracked_right_hand_point = None
 
+        #Variable to contain the custom message to send to 
         self.person_tracked_msg = PersonTracked()
          
-        self.image_height = None
+        """self.image_height = None
 
         self.image_width = None
 
         self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        self.video = cv2.VideoWriter('/media/maeri/UBUNTUUU/output1118.mp4',self.fourcc,20.0,(640,480))
+        self.video = cv2.VideoWriter('/media/maeri/UBUNTUUU/output1118.mp4',self.fourcc,20.0,(640,480))"""
 
 ################################ test subscriber #####################################################################################
-    def test_listener(self,frame_msg):
-        """To delete , just a test listener callback to verify the midpoint is really the midpoint"""
+    """def test_listener(self,frame_msg):
+        #To delete , just a test listener callback to verify the midpoint is really the midpoint
         frame = self.cv_bridge.imgmsg_to_cv2(frame_msg,'bgr8')
         self.image_height, self.image_width, _ = frame.shape
-        if self.person_tracked_middlepoint is not None:
-            self.video.write(cv2.circle(frame,(int(self.person_tracked_msg.middle_point.x),int(self.person_tracked_msg.middle_point.y)),10,(255,0,0),-1))
+        if self.person_tracked_midpoint is not None:
+            self.video.write(cv2.circle(frame,(int(self.person_tracked_msg.middle_point.x),int(self.person_tracked_msg.middle_point.y)),10,(255,0,0),-1))"""
 
-###########################first subscriber###########################################################################################   
+########################### First Subscriber ###########################################################################################   
     def bounding_boxes_listener_callback(self, boxes_msg):
-        """Callback function for the subscriber node (to topic /all_detected).
-        For each frame received, save in the log that an image has been received.
-        Then convert that image into cv2 format and keep that frame in a variable for further processing"""
-        
+        """Callback function for the subscriber node (to topic /all_bounding_boxes).
+        For each bounding box received, save it in a variable for processing"""
         self.get_logger().info('Bounding boxes received')
         self.boxes = boxes_msg
         
         
-########################### second subscriber #########################################################################################
+########################### Second Subscriber #########################################################################################
     def landmarks_listener_callback(self, lndmrk):
         """Callback function for the subscriber node (to topic /hand/landmarks).
         Receives a landmark from the hand gesture plugin and saves that landmark in a variable for further processing."""
-        
         self.get_logger().info('Landmark received')
         self.landmarks = lndmrk
         
 
-
-
 ######################### Publisher #####################################################################################################
     def person_tracked_callback(self):
-        """This function listens to the /hand/landmarks topic, and waits to spot the person who did the triggering move. I
-        n case a person did the trigger move, the function calls a publish the frames with only that person tracked. It sends the coordinates of the 
-        center of the bounding boxes around that person to person_tracker node, so that the latter node sends commands to the drone to follow that person."""
+        """This function listens to the /hand/landmarks topic, and waits to spot the person who did the triggering move. 
+        In case a person did the trigger move, the midpoint of the bounding box around that person is published on a the topic named /person_tracked. 
+        The last node (track_person.py) will subscribe to /person_tracked and send commands to the drone to follow the tracked person."""
 
-        if self.person_tracked_middlepoint is None and self.landmarks is not None :
-            if self.landmarks.right_hand.gesture == self.right_hand_gesture and self.landmarks.left_hand.gesture == self.left_hand_gesture:
-                self.get_logger().info("\n Open hand!! ")
+        if self.boxes is None:
+            self.get_logger().info("No bounding box received")
+        else:
+            if self.person_tracked_midpoint is None: #if no one has done the trigger move yet
+                if self.landmarks is not None :
+                    if self.landmarks.right_hand.gesture == self.right_hand_gesture and self.landmarks.left_hand.gesture == self.left_hand_gesture:
+                        self.get_logger().info("\n Tracking Trigger Move!!")
 
-                self.person_tracked_left_hand_point = self.landmarks.left_hand.normalized_landmarks[0]
-                self.person_tracked_right_hand_point = self.landmarks.right_hand.normalized_landmarks[0]
-                
-                self.person_tracked_middlepoint = PointMsg()
+                        #Saving the location of the hands of the person who did the move so that we can map him/her to a bounding box.
+                        #Only the points at the center if ther person's wrists are kept.
+                        self.person_tracked_left_hand_point = self.landmarks.left_hand.normalized_landmarks[0]
+                        self.person_tracked_right_hand_point = self.landmarks.right_hand.normalized_landmarks[0]
+                        
+                        #Instantiating the midepoint ROS message
+                        self.person_tracked_midpoint = PointMsg()
+                        #print("Bounding boxes",self.boxes.bounding_boxes,"\n\n")
+                        #self.video.write(cv2.line(self.image_all_detected,(int(middle_left.x*self.width),int(middle_left.y*self.height)),(int(middle_right.x*self.width),int(middle_right.y*self.height)),(255,0,0),4))
+                        
+                        #Find the bounding box around the person who did the trigger gesture, so that the midpoint of the box can be calculated
+                        self.find_bounding_box_of_tracked_person()
+                        
+                        self.person_tracked_msg.middle_point = self.person_tracked_midpoint #self.denormalize()  <--- This is useful if we want to send the real coordinates of the midpoint, and not coordinate within the range [0,1]
+                        
+                        if self.person_tracked_midpoint is not None:
+                            self.publisher_to_track.publish(self.person_tracked_msg) 
 
-                #print("\n Beware",self.person_tracked_left_hand_point)
-                #print(self.person_tracked_right_hand_point,"\n\n")
-
-                if self.boxes is None:
-                    self.get_logger().info("No bounding box received")
-                else:
-                    print("Bounding boxes",self.boxes.bounding_boxes,"\n\n")
-                #self.video.write(cv2.line(self.image_all_detected,(int(middle_left.x*self.width),int(middle_left.y*self.height)),(int(middle_right.x*self.width),int(middle_right.y*self.height)),(255,0,0),4))
-                self.find_bounding_box_of_tracked_person(self.person_tracked_left_hand_point,self.person_tracked_right_hand_point,self.boxes)
-                
-                self.person_tracked_msg.middle_point = self.denormalize()
-                
-                if self.person_tracked_middlepoint is not None:
-                    self.publisher_to_track.publish(self.person_tracked_msg) 
-
-        elif self.person_tracked_middlepoint is not None:
-            self.find_bounding_box_middlepoint()
-            self.person_tracked_msg.middle_point = self.denormalize()
-            self.publisher_to_track.publish(self.person_tracked_msg) 
-            print("\nNow we know the person to track. midpoint is ", self.person_tracked_middlepoint,"\n")
+            else: #if someone did the trigger move yet
+                self.find_bounding_box_middlepoint()
+                self.person_tracked_msg.middle_point = self.person_tracked_midpoint #self.denormalize() <--- This is useful if we want to send real coordinates of the midpoint, and not coordinate within the range [0,1]
+                self.publisher_to_track.publish(self.person_tracked_msg) 
+                print("\nNow we know the person to track. midpoint is ", self.person_tracked_midpoint,"\n")
 
     
     def denormalize(self)->PointMsg():
-        """Function to denormalize the corrdinates of the midpoint"""
+        """Function to denormalize the coordinates of the midpoint"""
         result = PointMsg()
-        result.x = self.person_tracked_middlepoint.x * self.image_width
-        result.y = self.person_tracked_middlepoint.y * self.image_height
+        result.x = self.person_tracked_midpoint.x * self.image_width
+        result.y = self.person_tracked_midpoint.y * self.image_height
         return result
 
-    def find_bounding_box_of_tracked_person(self, left_hand, right_hand, boxes):
-        """Finds the bounding box around the person who did the triggering move"""
-        for box in boxes.bounding_boxes:
+    def find_bounding_box_of_tracked_person(self):
+        """Finds the bounding box around the person who did the triggering move and updates self.person_tracked_midpoint
+        Precondition: self.boxes is not None"""
+        
+        #Loop to go through all the bounding boxes to find the box in which are contained both the left hand and right hand wrist center point
+        for box in self.boxes.bounding_boxes:
+
+            #Extracting the normalized coordinates of the top left and bottom right corner of each box. 
             top_left_x = box.top_left.x
             top_left_y = box.top_left.y
             bottom_right_x = box.bottom_right.x
             bottom_right_y = box.bottom_right.y
 
-            left_hand_x = left_hand.x
-            left_hand_y = left_hand.y
-            right_hand_x = right_hand.x
-            right_hand_y = right_hand.y
+            #Extracting the normalized coordinates of the left and right hand wrists' center points
+            left_hand_x = self.person_tracked_left_hand_point.x
+            left_hand_y = self.person_tracked_left_hand_point.y
+            right_hand_x = self.person_tracked_right_hand_point.x
+            right_hand_y = self.person_tracked_right_hand_point.y
 
+            #If both wrist' center points are in the bounding box, we update the midpoint 
             if top_left_x <= left_hand_x and top_left_x <= right_hand_x:
                 if bottom_right_x >= left_hand_x and bottom_right_x >= right_hand_x:
                     if top_left_y <= left_hand_y and top_left_y <= right_hand_y:
                         if bottom_right_y >= left_hand_y and bottom_right_y >= right_hand_y:
-                            self.person_tracked_middlepoint.x = top_left_x/ 2 + bottom_right_x/2 
-                            self.person_tracked_middlepoint.y = top_left_y/2 + bottom_right_y/2
-        #print("No bounding box found")
-        #return None
+                            self.person_tracked_midpoint.x = top_left_x/ 2 + bottom_right_x/2 
+                            self.person_tracked_midpoint.y = top_left_y/2 + bottom_right_y/2
+                            return 
+       
     
     def find_bounding_box_middlepoint(self):
-        """Find the nearest bounding box containing the middlepoint and upadates the middlepoint"""
+        """Find the nearest bounding box containing the middlepoint and updates the middlepoint.
+        This function is to use only when we already have a person to track (and hence, self.person_tracked_midpoint is not None)
+
+        Preconditions: self.person_tracked_midpoint is not None
+                      self.boxes is not None
+        """
+        #error between the actual midpoint (self.person_tracked_midpoint) and the midpoint of the bounding box
         smallest_error_margin = -1
+
+        #Loop to go through all the bounding boxes to find the bounding box whose midpoint is the closest to the midpoint of person tracked in the previous frame.
+        #The midpoint found will be the new midpoint of the person to track
         for box in self.boxes.bounding_boxes:
+            #Extracting the normalized coordinates of the top left and bottom right corner of each box.            
             top_left_x = box.top_left.x
             top_left_y = box.top_left.y
             bottom_right_x = box.bottom_right.x
             bottom_right_y = box.bottom_right.y
 
-            midpoint_x = self.person_tracked_middlepoint.x
-            midpoint_y = self.person_tracked_middlepoint.y
+            #Extracting the previous midpoint's coordinates
+            midpoint_x = self.person_tracked_midpoint.x
+            midpoint_y = self.person_tracked_midpoint.y
 
+            #if the previous midpoint is contained within the box, we calculate the distance between both points
+            #if that distance is the shortest encountered yet, we update the midpoint
             if top_left_x <= midpoint_x and top_left_y <= midpoint_y and bottom_right_x >= midpoint_x and bottom_right_y >= midpoint_y:
                 new_midpoint_x = top_left_x/ 2 + bottom_right_x/2
                 new_midpoint_y = top_left_y/2 + bottom_right_y/2
-                error_margin = self.euclidean_distance(midpoint_x,midpoint_y,new_midpoint_x,new_midpoint_y)
+                error_margin = self.euclidean_distance_squared(midpoint_x,midpoint_y,new_midpoint_x,new_midpoint_y) #distance between midpoints
                 
+                #updating the midpoint
                 if (smallest_error_margin == -1) or (error_margin < smallest_error_margin):
-                    self.person_tracked_middlepoint.x = new_midpoint_x
-                    self.person_tracked_middlepoint.y = new_midpoint_y
+                    self.person_tracked_midpoint.x = new_midpoint_x
+                    self.person_tracked_midpoint.y = new_midpoint_y
                     smallest_error_margin = error_margin
                 
 
 
-    def euclidean_distance(self,x1,y1,x2,y2):
+    def euclidean_distance_squared(self,x1,y1,x2,y2):
         """Calculates the eucliedean distance between two points (x1,y1) and (x2,y2)"""
         return (x2-x1)**2 + (y2-y1)**2
 
+
+"""
     def track_target(self,frame,id_of_interest):
-        """Function to track a person of a certain id only on a frame"""
+        #Function to track a person of a certain id only on a frame
 
         results = model.track(frame,persist=True) #stream = True
         index_of_target = 0
@@ -233,7 +237,7 @@ class TriggerTracking(Node):
             return frame_
         else:
             return frame
-
+"""
        
 	    
     
@@ -242,15 +246,18 @@ class TriggerTracking(Node):
 
 
 def main(args=None):
-    #Intialization ROS communication 
+    #Initialization ROS communication 
     rclpy.init(args=args)
+
+    #Node instantiation
     trigger_tracking = TriggerTracking('trigger_tracking_node')
 
-    #execute the callback function until the global executor is shutdown
+    #Execute the callback function until the global executor is shutdown
     rclpy.spin(trigger_tracking)
     
 
-    trigger_tracking.video.release()
+    #trigger_tracking.video.release()
+
     #destroy the node. It is not mandatory, since the garbage collection can do it
     trigger_tracking.destroy_node()
     
