26a1235947d136f93dd9e7e34871f1c772e75c9a
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2024-08-07 14:48:01 +0200

    Update track_person.py

--- person_tracking/person_tracking/person_tracking/track_person.py
+++ person_tracking/person_tracking/person_tracking/track_person.py
@@ -2,95 +2,120 @@
 import rclpy
 from rclpy.node import Node
 
-#ROS image message
-from sensor_msgs.msg import Image 
+from std_msgs.msg import Empty
+
+#ROS Twist message import. This message type is used to send commands to the drone.
+from geometry_msgs.msg import Twist
+
+
+#Custom message containing the midpoint of the bounding box surrounding the tracked person
+from person_tracked.msg import PersonTracked, PointMsg
 
 #For image manipulation (OpenCV)
 import cv2
-import numpy as np
+#import numpy as np
 
 #To convert cv2 images to ROS Image messages
 from cv_bridge import CvBridge
 
-#for object detection
-from ultralytics import YOLO
+from person_tracking.pid import PIDPoint
+
+class TrackPerson(Node):
+
+    
+    person_tracked_topic = "/person_tracked"
+    #bounding_boxes_topic = "/all_bounding_boxes"
+    commands_topic = "/cmd_vel" #carries Twist msgs
+    land_topic = "/land" #carries Empty msgs
 
 
+    image_height = 480
+    image_width = 640
 
-#load the object detection model
-model = YOLO('yolov8n.pt') 
-
-#Filtering our classes of interest
-classes = model.names
-classes_needed = ["cell phone"]
-classes_ID = [k for k,v in classes.items() if v in classes_needed]
-
-#Detection threshold probability
-minimum_prob = 0.4  
-
-
-class ImageSubscriberDetectedPublisher(Node):
-
+    
     def __init__(self,name):
         #Creating the Node
         super().__init__(name)
         
-        #subscriber
-        self.sub = self.create_subscription(Image,'image_raw',self.listener_callback,10)
-        
-        #publisher
-        self.publisher_ = self.create_publisher(Image, 'image_detected', 10)
-        self.timer = self.create_timer(0.1, self.timer_callback)
-        
-        self.cv_bridge = CvBridge()
-
-        #output video initialization
-        self.fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        self.video = cv2.VideoWriter('~/output.avi',self.fourcc,20.0,(640,480))
-
-        #Variable to read each frame
-        self.image=None
-
-    
-    def detection(self,frame):
-        """Function to perform object detection on frames"""
-        results = model.track(frame, persist=True, classes=classes_ID, conf=minimum_prob)
-        frame_ = results[0].plot()
-        return frame_
-        
-    def listener_callback(self, img):
-        """Callback function for the subscriber node (to topic image_raw).
-        For each image received, save in the log that an image has been received.
-        Then convert that image into cv2 format, perform detection on that image, 
-        and write the frame on the output video."""
-        self.get_logger().info('I saw an image')
-        image = self.cv_bridge.imgmsg_to_cv2(img,'rgb8')
-        self.image = self.detection(image)
-        self.video.write(np.array(self.image))
-          
-    def timer_callback(self):
-        """callback funtion for the publisher node (to topis image_detected).
-        The image on which object detection has been performed (self.image) is published on the topic 'image_raw'
-        """
-        self.publisher_.publish(self.cv_bridge.cv2_to_imgmsg(np.array(self.image), 'rgb8'))
+        #subscribers
+        self.sub_person_tracked = self.create_subscription(PersonTracked,self.person_tracked_topic, self.listener_callback,10)
+        #self.sub_landmark = self.create_subscription(Landmarks,self.hand_landmarks_topic, self.landmarks_listener_callback,10)
         
     
+        #publishers
+        self.publisher_commands = self.create_publisher(Twist,self.commands_topic,10)
+        self.timer_1 = self.create_timer(0.1, self.commands_callback)
         
+        #self.publisher_land = self.create_publisher(Empty,self.land_topic,10)
+        #self.timer_2 = self.create_timer(0.1, self.land_callback)
+
+        #self.cv_bridge = CvBridge()
+
+        #Variable to received bounding boxes containing all persons detected
+        #self.boxes = None
+        self.pid = PIDPoint((0.5, 0.5)) #middle of the screen for normalized midpoint coordinates
+       
+        self.person_tracked_midpoint = None
+
+        self.commands_msg = None
+
+        self.correction = None
+
+        
+###########################first subscriber###########################################################################################   
+    def listener_callback(self, msg):
+        """Callback function for the subscriber node (to topic /person_tracked).
+        Receives the midpoint of the boundign box surrounding the person tracked"""
+        
+        self.get_logger().info('Midpoint received')
+        self.person_tracked_midpoint = msg.middle_point
+        self.correction = self.pid.compute(self.person_tracked_midpoint)
+               
+######################### Publisher #####################################################################################################
+    def commands_callback(self):
+        """This function sends appropriate to the drone in order to keep the tracked person within the camera's field while ensuring safety"""
+        self.commands_msg = Twist()
+        
+        if self.correction is not None:
+            correction_x, correction_y = self.correction
+            if correction_x < -0.2 : #Here I don't put 0 to avoid having the drone always moving
+                #print("move left")
+                #self.commands_msg.li
+                print("move right")
+                self.commands_msg.linear.y -= 0.5
+
+            elif correction_x > 0.2 :
+                #print("move right")
+                print("move left")
+                self.commands_msg.linear.y += 0.5
+
+            if correction_y < -0.2 :
+                #print("move down")
+                print("move up")
+                self.commands_msg.linear.z += 0.5
+
+            elif correction_y > 0.2 :
+                #print("move up")
+                print("move down")
+                self.commands_msg.linear.z -= 0.5
+
+            self.publisher_commands.publish(self.commands_msg) 
+                    
+###################################################################################################################################       
+  
+
+
 def main(args=None):
     #Intialization ROS communication 
     rclpy.init(args=args)
-    
-    image_subscriber = ImageSubscriberDetectedPublisher('camera_subscriber_pub_detected')
+    track_person = TrackPerson('Track_Person_node')
 
     #execute the callback function until the global executor is shutdown
-    rclpy.spin(image_subscriber)
-
-    #release the video output writer.
-    image_subscriber.video.release()
-    image_subscriber.destroy_node()
+    rclpy.spin(track_person)
     
-    rclpy.shutdown()
+    track_person.video.release()
 
-
-if __name__ == '__main__':
-    main()
+    #destroy the node. It is not mandatory, since the garbage collection can do it
+    track_person.destroy_node()
+    
+    rclpy.shutdown()        
