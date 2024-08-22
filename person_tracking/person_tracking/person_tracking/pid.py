14327776f5305307c24703df4fceb268573fdcca
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2024-08-22 15:43:38 +0200

    Update pid.py

--- person_tracking/person_tracking/person_tracking/pid.py
+++ person_tracking/person_tracking/person_tracking/pid.py
@@ -15,18 +15,18 @@ class PIDPoint():
         self.error_y = 0
         
         self.kp = 1
-        self.ki = 1
-        self.kd = 1
+        self.ki = 0.01 #0 is the best
+        self.kd = 0
         self.time_step = 1 #frame
         self.output = 0 #correction to apply in the form (output_x, output_y)
 
     def compute(self,pos)->(float,float):
         """Funtion to compute the output of the PID controller, given the current
         position 'pos'. In our case pos represent the current coordinates of the middlepoint of tracked person"""
-        if self.integral_error_x > 3 :
-            self.integral_error_x = 0
-        if self.integral_error_y > 3:
-            self.integral_error_y = 0
+        #if self.integral_error_x > 3 :
+        #    self.integral_error_x = 0
+        #if self.integral_error_y > 3:
+        #    self.integral_error_y = 0
         setpoint_x, setpoint_y = self.setpoint
         pos_x = pos.x
         pos_y = pos.y
@@ -42,6 +42,9 @@ class PIDPoint():
         self.prev_error_y = self.error_y
 
         self.output = (self.kp * self.error_x + self.ki * self.integral_error_x + self.kd * self.derivative_error_x, self.kp * self.error_y + self.ki * self.integral_error_y + self.kd * self.derivative_error_y)
+        
+        #self.output = (self.kp * self.error_x + self.ki * self.integral_error_x, self.kp * self.error_y + self.ki * self.integral_error_y)
+
         return self.output
 
 
@@ -51,24 +54,31 @@ class PIDRange():
         self.setrange = (set_range_min,set_range_max)
         self.error = 0
         self.prev_error = 0
-        self.kp = 1
-        self.ki = 1
-        self.kd = 1
+        self.kp = 0.1#1
+        self.ki = 0.1#1
+        self.kd = 0#1
         self.time = 1 #frame
 
-"""
+
 class Point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 def main():
-    test_input = [Point(0.3,0.2),Point(0.5,0.5),Point(0.7,0.3),Point(0.8,0.5),Point(1,0.7),Point(0.5,0)]
+    #test_input = [Point(0.3,0.2),Point(0.1,0.2),Point(0.7,0.2)]#,Point(0.7,0.3),Point(0.8,0.5),Point(1,0.7),Point(0.5,0)]
     #test_input = [Point(0.3,0.3),Point(0.5,0.5),Point(0.7,0.7),Point(0.5,0.5),Point(1,1),Point(0,0)]
+
+    input_test = Point(0.3,0.2)
     test_pid = PIDPoint((0.5,0.5))
-    test_correction = map(test_pid.compute, test_input)
-    print(list(test_correction))
+    #test_correction = map(test_pid.compute, test_input)
+    #print(list(test_correction))
+
+    for i in range(50):
+        print(f"( {input_test.x} , {input_test.y} )")
+        new_error_x, new_error_y = test_pid.compute(input_test)
+        input_test = Point(input_test.x+new_error_x, input_test.y+new_error_y)
+        
 
 main()
 
-"""
